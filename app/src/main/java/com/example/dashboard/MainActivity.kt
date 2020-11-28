0198f0111476dd50b096e9d72d6d81de131a801c
JuiP <juipradhan2k@gmail.com>
2020-11-28 23:19:16 +0530

    implement setOnClickListener

--- app/src/main/java/com/example/dashboard/MainActivity.kt
+++ app/src/main/java/com/example/dashboard/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.dashboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
+
+private const val PACKAGE_NAME = "com.example.todoapp"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // get reference to button
+        val btn_click_todo = this.findViewById<Button>(R.id.TODO)
+        btn_click_todo.setOnClickListener {
+//            val intent = Intent()
+//            intent.setClassName(PACKAGE_NAME,“$PACKAGE_NAME.NotesActivity”)
+//            startActivity(intent)
+
+//            val launchIntent = packageManager.getLaunchIntentForPackage("com.example.todoapp")
+//            startActivity(launchIntent)
+
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component =
+                ComponentName("com.example.todoapp", "com.example.todoapp.NotesActivity")
+            startActivity(intent)
+            }
+        val btn_click_shopping = this.findViewById<Button>(R.id.Inventory)
+        btn_click_shopping.setOnClickListener {
+//            val intent = Intent()
+//            intent.setClassName(PACKAGE_NAME,“$PACKAGE_NAME.NotesActivity”)
+//            startActivity(intent)
+
+//            val launchIntent = packageManager.getLaunchIntentForPackage("com.example.todoapp")
+//            startActivity(launchIntent)
+
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component =
+                ComponentName("com.digitalwebandmoney.shoppinglistreminder", "com.digitalwebandmoney.shoppinglistreminder.StoresActivity")
+            startActivity(intent)
+        }
+        }
     }
-}
--- notes/src/main/java/com/example/todoapp/NotesActivity.kt
+++ notes/src/main/java/com/example/todoapp/NotesActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
